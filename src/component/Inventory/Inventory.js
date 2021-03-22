aa652cc95554ad00d853dab73df8c06a83bb7b37
Srabonifaria <76752669+Srabonifaria@users.noreply.github.com>
2021-03-22 20:43:36 +0600

    added more info

---
+++ src/component/Inventory/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Img from '../../images/hey.jpg'
+
+const ManageInventory = () => {
+    return (
+        <div style={{textAlign:'center'}}>
+            <h1 >Hello!!!</h1>
+            <img src={Img} alt=""/>
+        </div>
+    );
+};
+
+export default ManageInventory;
---
+++ src/component/ProductDetails/ProductDetails.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+// import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+
+const ProductDetails = () => {
+    const {productKey} =useParams() 
+    const [loading,setLoading] = useState(true)
+    const [product,setProduct] =useState({})
+    document.title="Product Details"
+    // const product = fakeData.find(pd => pd.key === productKey)
+    useEffect(() =>{
+        fetch('/product/' +productKey)
+        .then(res => res.json())
+        .then(data =>{
+            setProduct(data)
+            setLoading(false)
+        })
+    },[productKey])
+    return (
+        <div>
+            <h1>{productKey} Your Product Details</h1>
+            {
+                loading ? <p>loading...</p>:
+                <Product showAddToCart={false} product ={product}></Product>
+            }
+        </div>
+    );
+};
+
+export default ProductDetails;
